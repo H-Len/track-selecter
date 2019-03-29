608d1d747da6a72d8510f7681ad6b58a88f9e5dd
Hannah Melendy <hrmelendy@gmail.com>
2019-03-29 14:33:23 -0700

    add further alignment of code. working on functionality of branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
 $(document).ready(function() {
-  ("#user-data").submit(function(event) {
-    event.preventDefault();
-    var javaInput = $('#form-java').is(":checked");
-    var phpInput = $("#form-php").is(":checked");
-    var rubyInput = $('#form-ruby input:radio:checked').val();
-    $(".matches").hide();
+    $("#user-data").submit(function(event) {
+      event.preventDefault();
+      var javaInput = $('#form-java').is(":checked");
+      var phpInput = $("#form-php").is(":checked");
+      var rubyInput = $("#form-ruby").is(":checked");
+      $(".matches").hide();
 
-if (java){
- $("#javaSee").fadeIn();
-} else if (phpInput === false) {
- $(phpSee).slideIn();
-} else if (rubyInput === "classic") {
- $("#rubySee").fadeIn();
-} else (
-  <p>Start with ANY language offered and go from there.<p>
-);
+    if (javaInput){
+      $("#javaSee").fadeIn();
+    } else if (phpInput === true) {
+      $("#phpSee").slideIn();
+    } else if (rubyInput !== false) {
+      $("#rubySee").fadeIn();
+    } else {
+      $("#anyLang").show();
+    }
 
-
- });
-});
+    });
+  });
